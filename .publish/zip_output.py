d6277fb5b6ae815b3cf6277f3e6d924f6ce61e18
FrejaTerpPetersen <frtp@dhigroup.com>
2024-12-18 12:35:51 +0100

    Update zip script for outputs

--- .publish/zip_output.py
+++ .publish/zip_output.py
@@ -2,14 +2,13 @@
 
 import zipfile
 
-zip_fn = ".publish/MIKE21HD-ConceptionBay-output.zip"
+zip_fn = ".publish/MIKE21HD-SouthernNorthSea-output.zip"
 
 
 def zip_repo():
     with zipfile.ZipFile(zip_fn, "w") as z:
-        path_output = r"\\DKCPH1-STOR.DHI.DK\Projects\11560605\BenchmarkCases\WaterBench-MIKE21HD-ConceptionBay\MIKE21HD-ConceptionBay-output"
-        z.write(path_output + "/Area_2014_2018.dfsu",arcname="Area_2014_2018.dfsu")
-        z.write(path_output + "/Area_2019_2022.dfsu",arcname="Area_2019_2022.dfsu")
+        path_output = r"\\DKCPH1-STOR.DHI.DK\Projects\11560605\BenchmarkCases\WaterBench-MIKE21HD-SouthernNorthSea\MIKE21HD-SouthernNorthSea-output"
+        z.write(path_output + "/Area.dfsu",arcname="Area.dfsu")
         z.write(path_output + "/Points.dfs0",arcname="Points.dfs0")
         z.write(".publish/README_output.md",arcname="README.md")
     print(f"Zip file created: {zip_fn}")
